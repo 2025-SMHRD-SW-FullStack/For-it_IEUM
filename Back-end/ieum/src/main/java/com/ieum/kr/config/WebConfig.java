9f2c879acedfbcdd08336e614b2a2589b5dadec6
김형진 <clcjs987@gmail.com>
2025-07-01 08:24:02 +0900

    2025.07.01

--- Back-end/ieum/src/main/java/com/ieum/kr/config/WebConfig.java
+++ Back-end/ieum/src/main/java/com/ieum/kr/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.ieum.kr.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
-	 public void addCorsMappings(CorsRegistry registry) {
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/ieum/api/**")  // 또는 "/**"로 전체 허용
 	                .allowedOrigins("http://localhost:5173")  // React/Spring 사용하는 포트
 	                .allowedMethods("GET", "POST", "PUT", "DELETE")
