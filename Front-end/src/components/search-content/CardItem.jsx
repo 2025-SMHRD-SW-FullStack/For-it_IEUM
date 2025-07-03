0ba8df441df6e44d9e1903024166dad301d48cf8
YAY <yay315@naver.com>
2025-07-03 10:18:32 +0900

    아연0703

--- Front-end/src/components/search-content/CardItem.jsx
+++ Front-end/src/components/search-content/CardItem.jsx
@@ -21,19 +21,15 @@ const CardItem = ({ card }) => {
   }
 
   return (
-        <div 
-        className={`card ${isSelected ? 'selected': ''}`} 
-        key={card.id}
-        onClick={CardClick}
-        >
-          <div className='cardText'>
-            <div id="HS">HS코드: {card.hs_code}</div>
-            <div id="Name" title={card.product_name}>
-  품목명: {card.product_name.length > 6 ? card.product_name.slice(0, 6) + "..." : card.product_name}
-</div>
-
-          </div>
-          <img src={star} alt='별 아이콘' className='favorite'/>
+    <div
+      className={`card ${isSelected ? 'selected' : ''}`}
+      key={card.id}
+      onClick={handleCardClick}
+    >
+      <div className='cardText'>
+        <div id="HS">HS코드: {card.hs_code}</div>
+        <div id="Name" title={card.product_name}>
+          품목명: {card.product_name.length > 6 ? card.product_name.slice(0, 6) + "..." : card.product_name}
         </div>
       </div>
       <img
