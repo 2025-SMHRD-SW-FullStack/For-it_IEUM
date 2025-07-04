9b90b88cbde0f299d3909f774b688eac3f476612
ASM <nano000417@naver.com>
2025-07-04 16:08:05 +0900

    키워드 50% 완료

--- Front-end/src/components/search-sidebar/InterestKeyword.jsx
+++ Front-end/src/components/search-sidebar/InterestKeyword.jsx
@@ -2,13 +2,16 @@ import { useDrop } from 'react-dnd';
 import { toast } from 'react-toastify';
 import Keyword from './Keyword';
 
-const InterestKeyword = ({ interest, setInterest, Item }) => {
+const InterestKeyword = ({ interest, setInterest, Item ,userKeyword}) => {
+  const keywords = userKeyword || [];
+  
   const [, dropRef] = useDrop({
     accept: Item.KEYWORD,
     drop: (item) => {
       if (interest.includes(item.keyword)) return;
-
-      if (interest.length >= 5) {
+      console.log(`드래그 확인 ${userKeyword[0].productName}`);
+      
+      if (keywords.length >= 5) {
         toast.warn('관심 키워드는 최대 5개까지만 등록할 수 있어요.');
         return;
       }
@@ -17,19 +20,28 @@ const InterestKeyword = ({ interest, setInterest, Item }) => {
     },
   });
 
+  
+
+
   return (
     <div ref={dropRef} className="interestZone">
+      
       <b className="keywordLabel">관심 있는 키워드</b>
-      <br />
-      {interest.length === 0 && <p className="placeholder">여기로 드래그하세요</p>}
-      {interest.map((keyword, i) => (
+      <br/>
+      {keywords.length === 0 && <p className="placeholder">여기로 드래그하세요</p>}
+      
+      {keywords.map((keyword, i) => (
         <Keyword
           key={i}
-          keyword={keyword}
+          keywordId={keyword.id}
+          keyword={keyword.productName}
+          hsCode ={keyword.hsCode}
+          checkType={keyword.checkType}
           onClick={() => setInterest(interest.filter((k) => k !== keyword))}
           draggable={false}
         />
       ))}
+      
     </div>
   );
 };
