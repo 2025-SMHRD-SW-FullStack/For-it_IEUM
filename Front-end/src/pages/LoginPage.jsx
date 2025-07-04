b0b0ae5fe8e2aa8c42e07de378252dc045c593a5
ASM <nano000417@naver.com>
2025-07-04 09:04:18 +0900

    login Spring 연결(수정은 필요함)

--- Front-end/src/pages/LoginPage.jsx
+++ Front-end/src/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import AuthForm from '../components/User/AuthForm';
 import Social from '../components/User/SocialBtn';
 import './LoginPage.css';
+import { login } from '../services/userService';
 
 import naverLogo from '../assets/image/naver.png';
 import kakaoLogo from '../assets/image/kakao.jpg';
@@ -17,9 +18,30 @@ const LoginPage = () => {
     setForm((prev) => ({ ...prev, [name]: value }));
   };
 
+  // 현재 입력이 안되서 id, pasword 지정한거임 로그인 됨 
+  const user= 'nano'
+  const pw = '123'
+  const fetchResults = async () => {
+        try {
+          const response = await login({
+            userId:user,
+            password:pw,
+        });   // axios.get 결과
+          console.log(response);
+            localStorage.setItem('accessToken', response.result.token); // 해당 해더에 토큰 저장
+          // navigate('/search', { replace: true });
+          console.log(localStorage.getItem('accessToken'));
+        } catch (error) {
+          console.error('로그인:', error);
+        }
+      };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('로그인 시도:', form);
+
+    fetchResults(form);
+
     navigate('/');
   };
 
