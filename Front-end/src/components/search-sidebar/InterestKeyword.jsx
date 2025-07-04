aa5e2254de0ba1512b89ffe2c1ea0b78e89519b4
ASM <nano000417@naver.com>
2025-07-04 18:14:32 +0900

    키워드 저장 완료 중복 예외처리 완료

--- Front-end/src/components/search-sidebar/InterestKeyword.jsx
+++ Front-end/src/components/search-sidebar/InterestKeyword.jsx
@@ -9,7 +9,7 @@ const InterestKeyword = ({ interest, setInterest, Item ,userKeyword}) => {
   
   const [, dropRef] = useDrop({
     accept: Item.KEYWORD,
-    drop: (item) => {
+    drop:  (item) => {
       if (interest.includes(item.keyword)) return;
       // console.log(`드래그 확인 ${userKeyword[0].productName}`);
       
@@ -30,9 +30,9 @@ const InterestKeyword = ({ interest, setInterest, Item ,userKeyword}) => {
       
       <b className="keywordLabel">관심 있는 키워드</b>
       <br/>
-      {keywords.length === 0 && <p className="placeholder">여기로 드래그하세요</p>}
+      {keywords.length === 0 ? <p className="placeholder">여기로 드래그하세요</p>:
       
-      {keywords.map((keyword, i) => (
+      keywords.map((keyword, i) => (
         <Keyword
           key={i}
           keywordId={keyword.id}
